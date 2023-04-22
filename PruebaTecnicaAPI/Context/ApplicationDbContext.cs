dbe33599525cd30fceddd4e7c52d1341f6045a67
carlos hernandez <ch335037@gmail.com>
2023-04-21 19:16:07 -0600

    Agregar archivos de proyecto.

---
+++ PruebaTecnicaAPI/Context/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaTecnicaAPI.Models;
+
+namespace PruebaTecnicaAPI.Context
+{
+	public class ApplicationDbContext: DbContext
+	{
+		public DbSet<Rol> Roles { get; set; }
+
+		public DbSet<Usuario> Usuarios { get; set; }
+
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {
+		}
+	}
+}
---
+++ PruebaTecnicaAPI/Models/Rol.cs
@@ -0,0 +1,9 @@
+﻿namespace PruebaTecnicaAPI.Models
+{
+	public class Rol
+	{
+		public int Id { get; set; }
+		public string Nombre { get; set; }
+		public string Descripcion { get; set; }
+	}
+}
